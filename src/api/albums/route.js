41ac7f50193b85240114f389d4b84d40201afe02
Asadell <m.satrioasadell@gmail.com>
2024-07-19 09:53:17 +0700

    add: route

---
+++ src/api/albums/route.js
@@ -0,0 +1,28 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/albums',
+    handler: handler.addAlbumHandler,
+  },
+  // {
+  //   method: 'GET',
+  //   path: '/albums',
+  //   handler: handler.getAllAlbumsHandler,
+  // },
+  {
+    method: 'GET',
+    path: '/albums/{id}',
+    handler: handler.getAlbumByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/albums/{id}',
+    handler: handler.editAlbumByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/albums/{id}',
+    handler: handler.deleteAlbumByIdHandler,
+  },
+];
+module.exports = routes;
